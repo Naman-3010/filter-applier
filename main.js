eee14bf1af5bfccd3f7cae6652708a6cb022a660
Naman-3010 <91064945+Naman-3010@users.noreply.github.com>
2022-06-27 09:46:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function preload(){
 
 function setup(){
     canvas=createCanvas(640,480);
-    canvas.position(150,150);
+    canvas.position(420,300);
     video=createCapture(VIDEO);
     video.hide();
     tint_color="";
